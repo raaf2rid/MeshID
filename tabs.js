5905a710dd50365f0b1e4aaf3c70733995afbad4
raaf2rid <raaf2rid@gmail.com>
2023-06-04 23:48:33 +0600

    misc

--- tabs.js
+++ tabs.js
@@ -1,21 +1,11 @@
 const cardHeader = document.querySelector(".card-header");
 const card = document.querySelector(".card");
 const cardBody = document.querySelectorAll(".tab-pane");
-const head = document.querySelector("head");
 const navBar = document.querySelector(".card-header-tabs");
 const navItems = document.querySelectorAll(".nav-tabs > .nav-item");
 const navLinks = document.querySelectorAll(".nav-tabs> .nav-item > .nav-link");
-const builder = document.querySelectorAll(".builder-component");
 const cardHeaderUl = document.querySelector(".card-header > ul");
 const snackbar = document.querySelector("#snackbar");
-const tabs = document.querySelector(".wrapper");
-const formioWrapper = document.querySelector(".formio-wrapper");
-let parent = document.querySelector(".formio-component-form").parentElement;
-const child = document.querySelector(".formio-component-form");
-
-const errorFields = document.querySelectorAll(".tab-pane:not(:last-child) .custom-error")
-const backBtn = document.querySelectorAll('.navigation-back > .navigation-back')
-const nextBtn = document.querySelectorAll('.navigation-next > .navigation-next')
 const formio = document.querySelector('.preview .content .wrapper formio');
 
 
