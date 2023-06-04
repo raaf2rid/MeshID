58baf7835e7349dbdb08d085fa6d44eec6c72f12
raaf2rid <raaf2rid@gmail.com>
2023-06-04 15:05:17 +0600

    misc

---
+++ draft.js
@@ -0,0 +1,21 @@
+// Create a new <script> element
+let script = 'https://raw.githubusercontent.com/raaf2rid/MeshID/main/customization.js'
+
+
+
+fetch(script)
+  .then(response => response.text())
+  .then(scriptText => {
+    // Create a <script> element
+    const script = document.createElement('script');
+    
+    // Set the text content of the script
+    script.textContent = scriptText;
+    
+    // Append the script element to the <body> or any other element
+    document.body.appendChild(script);
+  })
+  .catch(error => {
+    // Handle error if the script could not be fetched or loaded
+    console.error('Error loading script:', error);
+  });
