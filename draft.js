e53cd14ba417cbf9b3f45d1f57d0dc20900943e4
raaf2rid <raaf2rid@gmail.com>
2023-06-05 01:52:27 +0600

    misc

--- draft.js
+++ draft.js
@@ -1,20 +1,13 @@
-  // Variable to track whether the form has been loaded
-let formLoaded = false;
-
-// Check if the form has not yet been loaded
-if (!formLoaded) {
+// Create a new <script> element
+let loadingScript = 'https://raw.githubusercontent.com/raaf2rid/MeshID/main/loading.js'
   // Perform the desired action
-  const preview = document.querySelector(".preview")
+
+if (!document.querySelector('script[src="' + loadingScript + '"]')) {
+const preview = document.querySelector(".preview")
 
 preview.style.display = 'none'
 
-
-// Create a new <script> element
-let script = 'https://raw.githubusercontent.com/raaf2rid/MeshID/main/loading.js'
-
-
-
-fetch(script)
+fetch(loadingScript)
   .then(response => response.text())
   .then(scriptText => {
     // Create a <script> element
@@ -31,8 +24,4 @@ fetch(script)
     console.error('Error loading script:', error);
   });
 
-  // Set the variable to true to indicate that the form has been loaded
-  formLoaded = true;
 }
-
-// If the form has already been loaded, nothing will happen
