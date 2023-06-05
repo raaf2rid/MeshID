bdf712097525074e35d4f696d9acc9e95e1af79b
raaf2rid <raaf2rid@gmail.com>
2023-06-05 13:14:25 +0600

    misc

--- draft.js
+++ draft.js
@@ -1,37 +1,25 @@
 // Check if the script has already been loaded
-if (!window.loading) {
+if (!window.customization) {
   // Set the flag to indicate that the script is being loaded
-  window.loading = true;
+  window.customization = true;
 
+  // Create a new <script> element
+  let customization = 'https://raw.githubusercontent.com/raaf2rid/MeshID/main/tabs.js';
 
-// Create a new <script> element
-let loadingScript = 'https://raw.githubusercontent.com/raaf2rid/MeshID/main/loading.js'
-  // Perform the desired action
-
-
-const preview = document.querySelector(".preview")
-
-preview.style.display = 'none'
-
-
-fetch(loadingScript)
-  .then(response => response.text())
-  .then(scriptText => {
-    // Create a <script> element
-    const script = document.createElement('script');
-    
-    // Set the text content of the script
-    script.textContent = scriptText;
-    
-    // Append the script element to the <body> or any other element
-    document.body.appendChild(script);
-  })
-  .catch(error => {
-    // Handle error if the script could not be fetched or loaded
-    console.error('Error loading script:', error);
-  });
-
+  fetch(customization)
+    .then(response => response.text())
+    .then(scriptText => {
+      // Create a <script> element
+      const script = document.createElement('script');
+      
+      // Set the text content of the script
+      script.textContent = scriptText;
+      
+      // Append the script element to the <body> or any other element
+      document.body.appendChild(script);
+    })
+    .catch(error => {
+      // Handle error if the script could not be fetched or loaded
+      console.error('Error loading script:', error);
+    });
 }
-
-
-
