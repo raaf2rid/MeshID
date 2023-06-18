48672d5f8000d592c8d05badf8c1bf5f6c3320c3
raaf2rid <raaf2rid@gmail.com>
2023-06-18 16:11:06 +0600

    misc

--- tabs.js
+++ tabs.js
@@ -149,14 +149,9 @@ const formio = document.querySelector('.preview .content .wrapper formio');
     navItems.forEach((item, i) => {
 
       item.style.width = "100%";
-      // item.style.borderBottom = "2px solid #F5F5F5"
-      if(i == 0){
-        item.classList.remove('active')
-        item.style.display = 'none'
-      }
-      if(i == 1){
-        item.classList.add('active')
-      }
+      navItems[0].style.display = 'none'
+      navItems[0].classList.remove('active')
+     
     });
   
     navLinks.forEach((item) => {
