7cbd67ff7df18f49ce04901bcad60c9a7a738bbe
raaf2rid <raaf2rid@gmail.com>
2023-06-26 18:57:31 +0600

    misc

--- tabs.js
+++ tabs.js
@@ -261,3 +261,18 @@ var headerCode = '<header class="header">' +
 
 // Prepend the header code to the '.card' element
 $('.card').prepend(headerCode);
+
+
+var btn = document.querySelector('.toggle');
+var btnst = true;
+btn.onclick = function() {
+  if(btnst == true) {
+    document.querySelector('.toggle span').classList.add('toggle');
+    document.getElementById('sidebar').classList.add('sidebarshow');
+    btnst = false;
+  }else if(btnst == false) {
+    document.querySelector('.toggle span').classList.remove('toggle');
+    document.getElementById('sidebar').classList.remove('sidebarshow');
+    btnst = true;
+  }
+}
