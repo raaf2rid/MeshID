f40bd94313cd25c2d39e5326fb13606bda285e3c
raaf2rid <raaf2rid@gmail.com>
2023-06-28 13:24:04 +0600

    misc

--- loading.js
+++ loading.js
@@ -1,6 +1,6 @@
 const previewElement = document.querySelector(".preview");
 const contentElement = document.querySelector(".content");
-const cssLink = document.querySelector('link[href="https://raaf2rid.github.io/MeshID/styles.css"]');
+let cssLink = document.querySelector('link[href="https://raaf2rid.github.io/MeshID/styles.css"]');
 const logo = document.querySelector(".preview .logo")
 
 // Check if the image has already been prepended
