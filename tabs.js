75884345a314ae903286e4d83b7e2c7f89b0c0a4
raaf2rid <raaf2rid@gmail.com>
2023-07-01 13:46:27 +0600

    misc

--- tabs.js
+++ tabs.js
@@ -293,7 +293,7 @@ btn.onclick = function() {
 
 
 radio.forEach(item=>{
-  if(item.querySelector('.custom-meshid-radio')){
+  if(!item.querySelector('.custom-meshid-radio')){
     item.querySelector('.form-check, .form-check-inline').classList.add('.custom-meshid-radio')
   }
 })
