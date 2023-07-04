455213a6badcedc98de186c28d8bde81f3ffbd18
raaf2rid <raaf2rid@gmail.com>
2023-07-04 15:03:01 +0600

    misc

--- sidebar.js
+++ sidebar.js
@@ -1,20 +1,12 @@
 
 const card = document.querySelector(".card");
 const cardBody = document.querySelectorAll(".tab-pane");
-const builder = document.querySelectorAll(".builder-component");
-const snackbar = document.querySelector("#snackbar");
-const tabs = document.querySelector(".wrapper");
-const formioWrapper = document.querySelector(".formio-wrapper");
-let parent = document.querySelector(".formio-component-form").parentElement;
-const child = document.querySelector(".formio-component-form");
 
 const cardHeader = document.querySelector(".card-header");
 const cardHeaderUl = document.querySelector(".card-header > ul");
 const navBar = document.querySelector(".card-header-tabs");
 const navItems = document.querySelectorAll(".nav-tabs > .nav-item");
 const navLinks = document.querySelectorAll(".nav-tabs> .nav-item > .nav-link");
-const formio = document.querySelector('.preview .content .wrapper formio');
-
 
 
 
@@ -25,90 +17,6 @@ if ($(".nav-header").length === 0) {
 $('li.nav-item a[href="#admin"]').parent('li.nav-item').removeClass('active');
 $('li.nav-item a[href="#admin"]').removeClass('active');
 
-
-
-navLinks.forEach((item) => {
-  
-  item.addEventListener("click", () => {
-    animate();
-  });
-});
-
-
-function animate() {
-
-
-  cardBody.forEach((tab,i)=>{
-    if(tab.classList.contains('active')){
-      navLinks[i].classList.add('active')
-      navItems[i].classList.add('active')
-      tab.classList.add("card-animation");
-      disableButtons();
-      setTimeout(removeAnimation, 800);
-    
-      function removeAnimation() {
-        enableButtons();
-        tab.classList.remove("card-animation");
-      }
-    }
-  })
-
-
-  
-}
-
-/////
-// Sidebar buttons enabling/disabling for Card animation to work properly
-/////
-
-function disableButtons() {
-  navLinks.forEach((item) => {
-    item.style.pointerEvents = "none";
-  });
-}
-
-function enableButtons() {
-  navLinks.forEach((item) => {
-    item.style.pointerEvents = "auto";
-  });
-}
-
-
-function mediaQueriesHeader(navHeader) {
-  /////
-  // Mobile Devices
-  /////
-
-  if (
-    window.matchMedia("(min-width: 200px)").matches &&
-    window.matchMedia("(max-width: 600px)").matches
-  ) {
-    navHeader.style.borderTopRightRadius = "5px";
-    navHeader.style.textAlign = "center";
-    navHeader.style.paddingLeft = "10px";
-    cardHeader.style.textAlign = "center";
-    snackbar.style.left = "37%";
-    cardHeader.style.borderBottom = "5px solid #f5f5f5";
-    cardHeader.style.borderRight = "0";
-    formio.style.width = '100%';
-  }
-
-  /////
-  // Large Screen
-  /////
-  else {
-    cardHeader.style.borderBottom = "0";
-    cardHeader.style.width = "570px";
-    card.style.minHeight = "670px";
-    navHeader.style.borderTopRightRadius = "0";
-    navHeader.style.textAlign = "start";
-    cardHeader.style.textAlign = "start";
-    navHeader.style.paddingLeft = "18px";
-    snackbar.style.left = "53%";
-    formio.style.width = '65%';
-  
-  }
-}
 
 
 $(document).ready(function() {
