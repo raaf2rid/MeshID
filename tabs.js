8423be146e3a4b7dc0cfbb299900f20761b10be1
raaf2rid <raaf2rid@gmail.com>
2023-07-04 11:33:38 +0600

    misc

--- tabs.js
+++ tabs.js
@@ -1,4 +1,3 @@
-// MeshID Custom
 const card = document.querySelector(".card");
 const cardBody = document.querySelectorAll(".tab-pane");
 const builder = document.querySelectorAll(".builder-component");
@@ -7,6 +6,12 @@ const tabs = document.querySelector(".wrapper");
 const formioWrapper = document.querySelector(".formio-wrapper");
 let parent = document.querySelector(".formio-component-form").parentElement;
 const child = document.querySelector(".formio-component-form");
+
+const cardHeader = document.querySelector(".card-header");
+const cardHeaderUl = document.querySelector(".card-header > ul");
+const navBar = document.querySelector(".card-header-tabs");
+const navItems = document.querySelectorAll(".nav-tabs > .nav-item");
+const navLinks = document.querySelectorAll(".nav-tabs> .nav-item > .nav-link");
 
 
 
