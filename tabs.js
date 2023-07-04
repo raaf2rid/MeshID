cc71ab916da52b2f149f94fa60c4a8fbc598ef7e
raaf2rid <raaf2rid@gmail.com>
2023-07-04 15:15:42 +0600

    misc

--- tabs.js
+++ tabs.js
@@ -10,16 +10,11 @@ const child = document.querySelector(".formio-component-form");
 const cardHeader = document.querySelector(".card-header");
 const cardHeaderUl = document.querySelector(".card-header > ul");
 const navBar = document.querySelector(".card-header-tabs");
+
 const navItems = document.querySelectorAll(".nav-tabs > .nav-item");
 const navLinks = document.querySelectorAll(".nav-tabs> .nav-item > .nav-link");
 
 
 const formio = document.querySelector('.preview .content .wrapper formio');
   
-  
-  
-    cardBody[0].classList.remove('active')
-    cardBody[0].style.display = 'none'
-    cardBody[1].classList.add('active')
-    cardBody[1].style.display = 'block'
-    
+
