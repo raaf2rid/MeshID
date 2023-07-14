bdcc175148115681fa4dea1f57a7667f793dc77c
raaf2rid <raaf2rid@gmail.com>
2023-07-14 20:19:52 +0600

    misc

--- Form/sidebar.js
+++ Form/sidebar.js
@@ -1,21 +1,20 @@
-// Hover Animation 
+// Hover Animation
 
-$(function() {
-  $('.nav-link').hover(function() {
-    $(this).parent('.nav-item').addClass('hovered');
-  }, function() {
-    $(this).parent('.nav-item').removeClass('hovered');
-  });
+$(function () {
+  $(".nav-link").hover(
+    function () {
+      $(this).parent(".nav-item").addClass("hovered");
+    },
+    function () {
+      $(this).parent(".nav-item").removeClass("hovered");
+    }
+  );
 });
 
+// Hamburger Icon
 
-
-// Hamburger Icon 
-
-
-if ($('.card .header').length === 0) {
-  let headerCode = 
-  `<header class="header">
+if ($(".card .header").length === 0) {
+  let headerCode = `<header class="header">
       <div class="switch-holder">
       <div class="switch-label">
       <i class="fa-solid fa-paperclip" style="color: #383838;"></i><span>All Pages</span>
@@ -27,79 +26,88 @@ if ($('.card .header').length === 0) {
     </div>
   </header>`;
 
-    $('.card').prepend(headerCode);
+  $(".card").prepend(headerCode);
 }
-
 
 // Sidebar Show/Hide
 
-$('.switch-toggle input[type="checkbox"]').click(function() {
-  let sidebar = $('.card-header');
-  let toggleIcon = $('.switch-holder');
-  sidebar.toggleClass('sidebarshow');
-  toggleIcon.toggleClass('toggled');
+$('.switch-toggle input[type="checkbox"]').click(function () {
+  let sidebar = $(".card-header");
+  let toggleIcon = $(".switch-holder");
+  sidebar.toggleClass("sidebarshow");
+  toggleIcon.toggleClass("toggled");
 });
 
-$('.card-header .nav-link').each(function() {
-  var clonedElement = $(this).clone().removeAttr('href');
+
+// Cloning and replacing sidebar links 
+
+$(".card-header .nav-link").each(function () {
+  var clonedElement = $(this).clone().removeAttr("href");
   $(this).replaceWith(clonedElement);
 });
 
 
 
-$('.card-header .nav-link').click(function() {
-  const clickedIndex = $(this).parent().index();
-  
-  $('.card-header .nav-link').removeClass('active').parent().removeClass('active');
-  $(this).addClass('active').parent().addClass('active');
+// Show/Hide active Tab 
 
-  const activeTab = $('.tab-pane.active');
-  const nextTab = $('.tab-pane').eq(clickedIndex);
+$(".card-header .nav-link").click(function () {
+  const clickedIndex = $(this).parent().index();
+
+  $(".card-header .nav-link")
+    .removeClass("active")
+    .parent()
+    .removeClass("active");
+  $(this).addClass("active").parent().addClass("active");
+
+  const activeTab = $(".tab-pane.active");
+  const nextTab = $(".tab-pane").eq(clickedIndex);
 
   const activeTabHeight = activeTab.outerHeight();
   const nextTabHeight = nextTab.outerHeight();
 
   activeTab.height(activeTabHeight);
-  activeTab.slideUp('slow', function() {
-    activeTab.removeClass('active');
-    nextTab.css({
-      display: 'block',
-      height: 0
-    }).animate(
-      {
-        height: nextTabHeight
-      },
-      {
-        duration: 'slow',
-        complete: function() {
-          nextTab.addClass('active').css('height', '');
+  activeTab.slideUp("slow", function () {
+    activeTab.removeClass("active");
+    nextTab
+      .css({
+        display: "block",
+        height: 0,
+      })
+      .animate(
+        {
+          height: nextTabHeight,
+        },
+        {
+          duration: "slow",
+          complete: function () {
+            nextTab.addClass("active").css("height", "");
+          },
         }
-      }
-    );
+      );
   });
 });
 
 
+
+// Toggle Tab Names on the Sidebar
+
 function updateSidebar() {
-  $('.tab-pane').each(function(index) {
-    const hasHiddenClass = $(this).children().first().hasClass('formio-hidden');
-    const navItem = $('.card-header .nav-item').eq(index);
-    const animationOptions = { duration: 'fast', easing: 'linear' };
-    hasHiddenClass ? navItem.slideUp(animationOptions) : navItem.slideDown(animationOptions);
+  $(".tab-pane").each(function (index) {
+    const hasHiddenClass = $(this).children().first().hasClass("formio-hidden");
+    const navItem = $(".card-header .nav-item").eq(index);
+    const animationOptions = { duration: "fast", easing: "linear" };
+    hasHiddenClass
+      ? navItem.slideUp(animationOptions)
+      : navItem.slideDown(animationOptions);
   });
 }
 
 updateSidebar();
 
-
-const $card = $('.card');
+const $card = $(".card");
 
 const observer = new MutationObserver(updateSidebar);
 
 const observerConfig = { childList: true, subtree: true };
 
 observer.observe($card[0], observerConfig);
-
-
-
-
